226e121fa63435429b86c310abfd72255bf61783
iremcingitas1287-creator <iremcingitas1287@gmail.com>
2026-05-05 10:12:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import json
 from datetime import datetime
-%%writefile app.py
 import streamlit as st
 import pandas as pd
 from supabase import create_client
